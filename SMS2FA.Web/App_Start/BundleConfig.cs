52de96f38e121d36056fb54e990d2c350ce16187
Agustin Camino <agustin.camino@gmail.com>
2016-05-25 15:49:08 -0500

    Update UI

--- SMS2FA.Web/App_Start/BundleConfig.cs
+++ SMS2FA.Web/App_Start/BundleConfig.cs
@@ -23,8 +23,9 @@ namespace SMS2FA.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
