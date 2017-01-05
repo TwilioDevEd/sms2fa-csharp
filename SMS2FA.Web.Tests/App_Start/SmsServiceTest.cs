36d97a5acde199499a827614ac63a8fb112bb6be
Hector Ortega <hector.oj@gmail.com>
2017-01-05 14:26:39 -0500

    Fix code and tests after upgrading Twilio lib

--- SMS2FA.Web.Tests/App_Start/SmsServiceTest.cs
+++ SMS2FA.Web.Tests/App_Start/SmsServiceTest.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using Moq;
 using NUnit.Framework;
-using System.Threading.Tasks;
-using Twilio;
-using WarmTransfer.Web.Domain;
+using Twilio.Clients;
+using Twilio.Http;
 
 namespace SMS2FA.Web.Tests.App_Start
 {
@@ -12,9 +12,14 @@ namespace SMS2FA.Web.Tests.App_Start
         [Test]
         public async Task WhenSendAsync_MessageIsSent()
         {
-            var mockClient = new Mock<TwilioRestClient>(string.Empty, string.Empty);
+            var mockClient = new Mock<ITwilioRestClient>();
             var smsService = new SmsService(mockClient.Object);
-
+            mockClient
+                .Setup(c => c.RequestAsync(It.Is<Request>(
+                    r => r.Method == HttpMethod.Post &&
+                    r.ConstructUrl().AbsoluteUri.Contains("Messages.json"))
+                    ))
+                .Returns(Task.FromResult(new Response(System.Net.HttpStatusCode.OK, "")));
             var identityMessage = new IdentityMessage
             {
                 Destination = "+555 555",
@@ -22,8 +27,10 @@ namespace SMS2FA.Web.Tests.App_Start
             };
             await smsService.SendAsync(identityMessage);
 
-            mockClient.Verify(
-                c => c.SendMessage(Config.TwilioNumber, identityMessage.Destination, identityMessage.Body), Times.Once);
+            mockClient.Verify(c => c.RequestAsync(It.Is<Request>(
+                    r => Equals(r.Method, HttpMethod.Post) &&
+                    r.ConstructUrl().AbsoluteUri.Contains("Messages.json")) 
+                    ), Times.Once);
         }
     }
 }
